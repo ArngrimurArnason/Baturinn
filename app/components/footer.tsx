eb0e1422d2a1973a1f7c1972fa4b0f072fb13e1d
Arngrimur <arngrimurarnason@gmail.com>
2026-01-26 23:14:53 +0000

    Initial commit

---
+++ app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-900 text-white mt-auto">
+       <text> BYEEE</text>
+    </footer>
+  );
+}
---
+++ app/components/nav.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-900 text-white shadow-lg">
+      <text> HELLOO</text>
+    </nav>
+  );
+}
